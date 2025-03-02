589d8d06a5db28f4d8835a075a64069c2cbaac98
patrignatenko <patrik.ignatenko@vikk.ee>
2025-03-02 12:40:41 +0200

    pygame3

--- Ülesanne 3 Tsüklid ja funktsioonid.py
+++ Ülesanne 3 Tsüklid ja funktsioonid.py
@@ -1,41 +1,30 @@
 import pygame
 
-# PyGame algatus
 pygame.init()
-
-# Akna loomine
 screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption("My Screen")
 
-# Värvid
-valge = (255, 255, 255)
-must = (0, 0, 0)
+roheline = (153, 255, 153)
+punane = (255, 0, 0)
 
-# Funktsioon ruudustiku joonistamiseks
+
 def draw_grid(square_size, rows, cols, line_color):
     for row in range(rows):
         for col in range(cols):
-            # Arvutame ruudu koordinaadid
             x = col * square_size
             y = row * square_size
-            # Joonistame ruudu
             pygame.draw.rect(screen, line_color, (x, y, square_size, square_size), 1)
 
-# Tsükkel
+
 running = True
 while running:
-    screen.fill(valge)  # Täidame ekraani taustavärviga
+    screen.fill(roheline)
+    draw_grid(20, screen.get_height() // 20, screen.get_width() // 20, punane)
 
-    # Joonistame ruudud (ruudu suurus, ridade arv, veergude arv, joone värv)
-    draw_grid(20, screen.get_height() // 20, screen.get_width() // 20, must)
-
-    # Kontrollime, kas akent suletakse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     pygame.display.flip()
 
-
 pygame.quit()
-
